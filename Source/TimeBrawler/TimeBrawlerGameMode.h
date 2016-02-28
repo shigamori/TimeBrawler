72bd738099e134f04e2276dc9ca88b55dc1f808d
Shigamori <shigamori.james@gmail.com>
2016-02-28 17:17:16 -0500

    First Upload

---
+++ Source/TimeBrawler/TimeBrawlerGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TimeBrawlerGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TIMEBRAWLER_API ATimeBrawlerGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
